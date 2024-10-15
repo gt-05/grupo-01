0a7c06bcf0a6cc5b45384c0e07ef28f43cad17d3
unknown <LETICIA6211@DIGITALCOLLEGE.LOCAL>
2024-10-15 11:44:01 -0300

    Adicionando documentos com os cards de coleçoes em destaques

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import CardSale from "./components/cardssale"
 import Footer from "./components/footer"
 
 function App() {
-  
+
 
   return (
     <>
-      <Footer/>
+      <CardSale />
+      <Footer />
     </>
   )
 }
